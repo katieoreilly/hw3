1656d75ff8ba994b4e4b3b0375607eb1351ef855
Katie O'Reilly <katie.oreilly@kellogg.northwestern.edu>
2024-02-15 20:59:15 +0000

    update 1

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,22 @@
+class PlacesController < ApplicationController
+ 
+  def index
+    @places = Place.all 
+  end
+  
+  def show
+    @place = Place.find_by({"id" => params["id"]})
+  end
+
+  def new
+    # render view with new Place form
+  end
+
+  def create
+    @place = Place.new
+    @place["name"] = params["name"]
+    @place.save
+    redirect_to "/places"
+  end 
+
+end
