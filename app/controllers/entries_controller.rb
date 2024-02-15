a6762b284ed3ea20b9b752b58db3c818c50381b2
Katie O'Reilly <katie.oreilly@kellogg.northwestern.edu>
2024-02-15 22:58:14 +0000

    phase 2

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,4 +4,24 @@ class EntriesController < ApplicationController
     @entry = Entry.all 
   end
 
+  def show
+    @entry = Entry.find_by({"id" => params["id"]})
+  end
+
+  def new
+    # render view with new Entry form
+  end
+
+  def create
+    @entry = Entry.new
+    @entry["title"] = params["title"]
+    @entry["description"] = params["description"]
+    @entry["posted_on"] = params ["posted_on"]
+
+    @entry["place_id"] = params["place_id"]
+
+    @entry.save
+    redirect_to "/places"
+  end 
+
 end
