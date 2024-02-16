393fcfa8f982fca32b3d9bbe3b99741f068abdd5
Katie O'Reilly <katie.oreilly@kellogg.northwestern.edu>
2024-02-16 02:23:12 +0000

    workaround

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,10 +3,6 @@ class PlacesController < ApplicationController
   def index
     @places = Place.all 
   end
-  
-  def show
-    @place = Place.find_by({"id" => params["id"]})
-  end
 
   def new
     # render view with new Place form
@@ -14,9 +10,18 @@ class PlacesController < ApplicationController
 
   def create
     @place = Place.new
+
     @place["name"] = params["name"]
+
     @place.save
+    
     redirect_to "/places"
   end 
+  
+  def show
+    @place = Place.find_by({"id" => params["id"]})
+  end
+
+  
 
 end
