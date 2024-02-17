ad37dbd98532a1f312269a2e1937ac15dfaba7e4
Katie O'Reilly <katie.oreilly@kellogg.northwestern.edu>
2024-02-17 01:33:54 +0000

    final touches

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources "places"
-  get("/", { :controller => "things", :action => "index" })
-
   resources "entries"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
